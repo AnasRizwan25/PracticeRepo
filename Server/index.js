db599532839f69c9618597ca91deca84b4bfc0f0
smfahad19 <syedfahad305171@gmail.com>
2025-11-14 20:57:56 +0500

    Added .gitignore to ignore node_modules

---
+++ Server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import "./config/db.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+
+app.listen(5000, () => {
+  console.log("Server Running on port 5000");
+});
